124c500ee14423bf59959ac09dff7561dda656ac
Sukhesh Patro <sukheshpatro@gmail.com>
2025-06-23 16:48:53 +1000

    init commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/src/components/EditScreenInfo';
+import { Text, View } from '@/src/components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-2xl font-bold'>Tab One HIIIII</Text>
+      <View className='w-[80%] h-1 m-30' lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
